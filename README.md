44f8b465998f92385bd87515d083e5f836b24f11
Alex Nikolova <a.nikolova@aurainfosec.com>
2020-06-24 14:31:18 +1200

    First commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# Chroot with a fake root user
+
+This script creates a fake root where your user has write permission to arbitrary locations. It does not require any dependencies.
+
+## Why do it?
+
+I wrote this when I had to download and extract a Linux package and run it as a non-root user. Trouble was the package was dynamically loading its shared libraries from `/usr/lib64`, ignoring the `LD_LIBRARY_PATH` variable. So the only way I could run it was inside a chroot where I could copy its libraries to the system location. But of course to create a functioning chroot you need to do one of the following things:
+
+* Copy over most directories, such as `/etc`, `/dev` and others to the new fake root...
+	
+	Not efficient, would take a lot of space and probably fail for `/dev`, `/proc` and `/sys`.
+	
+* Mount an overlay filesystem into the fake root
+
+	Ideal solution, except... it requires `root`
+	
+* Bind mount most directories
+
+	Good, but again it requires root... or so I thought!
+	
+	Turns out, in **some** cases [you can trick mount into thinking you're root](https://mostlyuseful.tech/posts/overlay-mounting/). The article I'm linking to suggests it should work with overlay filesystems, but at least on Amazon Linux it doesn't: it still fails with permission error. However, it works with bind mounts. So I wrote this script.
+	
+## What it does
+
+It takes a list of directories or files which you want to be writable to your fake `root` user inside your fake root directory, and bind mounts everything else (for files it creates hard links).
+
+For example if you want a fake root where your fake `root` user has write access to `/usr/bin`, `/usr/lib64` and `/etc/hosts`, call the script with:
+
+```
+fake-root-chroot.sh -w /etc/hosts -w /usr/lib64 -w /usr/bin /path/to/chroot
+```
+
+The script will create the following structure:
+
+`/etc ->` a new directory owned by the fake `root` user
+
+`/etc/hosts ->` a copy of the original `/etc/hosts`, now owned by the fake `root` user
+
+`/etc/* ->` all directories are bind-mounted, all other files are hard-symlinked
+
+`/usr/bin ->` same as for `/etc`
+
+`/* ->` all directories are bind-mounted, all files are hard-symlinked 
---
+++ fake-root-chroot.sh
@@ -0,0 +1,192 @@
+#!/bin/bash
+
+if [[ "${UID}" != 0 ]] ; then
+  unshare -rm "${BASH_SOURCE[0]}" "${@}"
+  exit $?
+fi
+
+usage() {
+  cat <<EOF
+${ANSI_BRIGHT}${ANSI_REVERSE}Usage:${ANSI_RESET}
+${ANSI_BRIGHT}${BASH_SOURCE[0]}${ANSI_RESET} ${ANSI_FG_CYAN}[<options>]${ANSI_RESET} ${ANSI_BRIGHT}${ANSI_FG_GREEN}[<chroot dir>]${ANSI_RESET}
+
+${ANSI_BRIGHT}${ANSI_REVERSE}Options:${ANSI_RESET}
+  ${ANSI_FG_CYAN}-n      ${ANSI_RESET}Dry run, only print what would be done (non-verbose by default)
+  ${ANSI_FG_CYAN}-v      ${ANSI_RESET}Verbose, print all commands that are/would be invoked
+  ${ANSI_FG_CYAN}-w DIR  ${ANSI_RESET}Add DIR to the lsit of directories which need to be writable to the
+          fake root
+EOF
+exit 0
+}
+
+function log() {
+  _log "" "${@}"
+}
+
+function logn() {
+  _log "-n" "${@}"
+}
+
+function _log() {
+  local echoargs="${1}" level="${2}" colors ansi_col
+  shift 2
+  colors_v="ANSI_${level}"
+  read -a colors <<<"${!colors_v}"
+  for c in "${colors[@]}"; do
+    col="ANSI_${c}"
+    ansi_col+="${!col}"
+  done
+  echo ${echoargs} "${ansi_col}${*}${ANSI_RESET}"
+}
+
+bind_mount() {
+  for target in "${@}" ; do
+    for wrt in "${WRITABLES[@]}" ; do
+      if [[ "${wrt}" == "${target}" || -d "${target}" && "${wrt#${target}/}" != "${wrt}" ]] ; then
+        target=".orig_${target}"
+      fi
+    done
+    src="${target#.orig_}"
+    dest="${CHROOT}${target}"
+    if [[ "${src}" != "${target}" ]] ; then
+      if [[ -d "${src}" ]] ; then
+        log INFO "${src} contains locations which should be writable"
+        cmd mkdir "${CHROOT}${src}"
+        bind_mount "${src}"/* || return 1
+      else
+        log INFO "${src} should be writable, copying"
+        cmd cp "${src}" "${dest}" || return 1
+      fi
+      continue
+    fi
+
+    if [[ -d "${src}" ]] ; then
+      cmd mkdir -p "${dest}" || return 1
+      cmd chmod --reference="${src}" "${dest}" || return 1
+      MOUNTED+=("${dest}")
+      cmd mount --rbind "${src}" "${dest}" || return 1
+    else
+      cmd ln "${src}" "${dest}" || return 1
+    fi
+  done
+}
+
+cmd() {
+  if [[ "${VERBOSE}" -eq 1 ]] ; then
+    log DEBUG "${@}"
+  fi
+  if [[ "${DRY_RUN}" -eq 0 ]] ; then
+    "${@}"
+  else
+    true
+  fi
+}
+
+############################################################
+ANSI_RESET=$'\x1b[0m'
+ANSI_BRIGHT=$'\x1b[1m'
+ANSI_DIM=$'\x1b[2m'
+ANSI_UNDERSCORE=$'\x1b[4m'
+ANSI_BLINK=$'\x1b[5m'
+ANSI_REVERSE=$'\x1b[7m'
+ANSI_HIDDEN=$'\x1b[8m'
+ 
+ANSI_FG_BLACK=$'\x1b[30m'
+ANSI_FG_RED=$'\x1b[31m'
+ANSI_FG_GREEN=$'\x1b[32m'
+ANSI_FG_YELLOW=$'\x1b[33m'
+ANSI_FG_BLUE=$'\x1b[34m'
+ANSI_FG_MAGENTA=$'\x1b[35m'
+ANSI_FG_CYAN=$'\x1b[36m'
+ANSI_FG_WHITE=$'\x1b[37m'
+ 
+ANSI_BG_BLACK=$'\x1b[40m'
+ANSI_BG_RED=$'\x1b[41m'
+ANSI_BG_GREEN=$'\x1b[42m'
+ANSI_BG_YELLOW=$'\x1b[43m'
+ANSI_BG_BLUE=$'\x1b[44m'
+ANSI_BG_MAGENTA=$'\x1b[45m'
+ANSI_BG_CYAN=$'\x1b[46m'
+ANSI_BG_WHITE=$'\x1b[47m'
+
+ANSI_DEBUG="FG_BLUE"
+ANSI_INFO="BRIGHT FG_WHITE"
+ANSI_ERROR="BRIGHT FG_WHITE BG_RED"
+
+VERBOSE=0
+DRY_RUN=0
+CHROOT=
+WRITABLES=()
+
+############################################################
+while [[ $# -gt 0 ]] ; do
+  case "${1}" in
+    -n)
+      DRY_RUN=1
+      ;;
+    -v)
+      VERBOSE=1
+      ;;
+    -h)
+      usage
+      ;;
+    -w)
+      if [[ -d "${target}" ]] ; then
+        # ensure trailing /
+        WRITABLES+=("${2%/}/")
+      else
+        WRITABLES+=("${2}")
+      fi
+      shift
+      ;;
+    -*)
+      log ERROR "Unknown option ${1}"
+      usage
+      ;;
+    *)
+      if [[ -n "${CHROOT}" ]] ; then
+        log ERROR "Extra argument ${1}"
+        usage
+      fi
+      CHROOT="${1%/}"
+  esac
+  shift
+done
+
+############################################################
+if [[ -z "${CHROOT}" ]] ; then
+  logn INFO "Assuming that current working directory is the chroot, is this correct? (y/n) "
+  read -n 1 ans
+  echo ""
+  while [[ ! "${ans}" =~ [yYnN] ]] ; do
+    logn INFO "Answer with y or n "
+    read -n 1 ans
+    echo ""
+  done
+  if [[ "${ans}" =~ [nN] ]] ; then
+    exit 1
+  fi
+  CHROOT=.
+fi
+
+if test -n "$(shopt -s nullglob; echo "${CHROOT}"/*)" ; then
+  log ERROR "chroot directory must be empty, aborting." >&2
+  exit 1
+fi
+
+log DEBUG "Writables:" "${WRITABLES[@]}"
+shopt -s dotglob
+MOUNTED=()
+cmd mkdir -p "${CHROOT}"
+bind_mount /*
+rc=$?
+cmd chroot "${CHROOT}"
+log INFO "Cleaning up"
+for m in "${MOUNTED[@]}" ; do
+  cmd umount --lazy "${m}"
+  this_rc=$?
+  if [[ "${rc}" -eq 0 ]] ; then
+    rc="${this_rc}"
+  fi
+done
+find "${CHROOT}" \( ! -type d -o -empty \) -delete
